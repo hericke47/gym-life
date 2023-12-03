0f3f2af6fc52236fd061ec8c8fc0671292407243
Herick Exterkoetter <herickherick47@gmail.com>
2023-12-03 04:06:49 -0300

    feat: adiciona middleware para verificar se usuário é admin

---
+++ Backend/src/shared/infra/http/middlewares/ensureAdmin.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express";
+
+import UserRepository from "@modules/users/infra/typeorm/repositories/UserRepository";
+import AppError from "@shared/errors/AppError";
+
+export default async function ensureAdmin(
+  request: Request,
+  _response: Response,
+  next: NextFunction
+) {
+  const { id } = request.user;
+
+  const userRepository = new UserRepository();
+  const user = await userRepository.findById(id);
+
+  if (!user?.is_admin) {
+    throw new AppError("User isn't admin");
+  }
+
+  return next();
+}
